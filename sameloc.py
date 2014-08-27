892d278afe5fc62b6c647d0bb17d67421130ecc6
lgadsden <lgadsden@email.gwu.edu>
2014-08-26 21:07:01 -0400

    Update sameloc.py
    made changes to way strings were written.

--- sameloc.py
+++ sameloc.py
@@ -6,49 +6,49 @@ location = r"D:\OCR_FIN\SPEC\MV BULLETINS"
 def move_sameloc(path): #moves folders that were not bagged (do not have a data folder)
 	for item in os.listdir(path):
 		ofolder = "%s\\%s" %(location, item)
-		imagefolder = ofolder + "\\IMAGES_METADATA"
-		datafolder = ofolder + "\\data"
+		imagefolder = "%s\\IMAGES_METADATA" %(ofolder)
+		datafolder = "%s\\data" %(ofolder)
 		
 		for folder in os.listdir(ofolder):
-			old_loc = str(ofolder) + "\\" + str(folder)
+			old_loc = "%s\\%s" %(ofolder,folder)
 			print old_loc
 			if old_loc.endswith("IMAGES"):
 				 shutil.move(old_loc, imagefolder)
-				 print str(old_loc) + " moved to " + str(imagefolder)
+				 print "%s moved to %s" %(olf_loc,imagefolder)
 			elif old_loc.endswith("IMAGES_CR2"):
 				 shutil.move(old_loc, imagefolder)
-				 print str(old_loc) + " moved to " + str(imagefolder)
+				 print "%s moved to %s" %(old_loc,imagefolder)
 			elif old_loc.endswith("METADATA"):
 				 shutil.move(old_loc, imagefolder)
-				 print str(old_loc) + " moved to " + str(imagefolder)		
+				 print "%s moved to %s" %(old_loc,imagefolder)		
 			else:
 				print "there is nothing else to do"
 
 # if 2 PDFS exist this will create an access folder and move the access copy there 
 def move_pdf(path): 
 	for folder in os.listdir(path):
-		ofolder = str(path) + "\\" + str(folder)
+		ofolder = "%s\\%s" %(path,folder)
 		for subfolder in os.listdir(ofolder):
 			if subfolder == 'PDF':
-				pdf_folder = str(ofolder) + "\PDF"
+				pdf_folder = "%s\PDF" %(ofolder)
 				for PDF in os.listdir(pdf_folder):
-					access_loc = str(ofolder) + "\ACCESS_PDF"
+					access_loc = "%s\\ACCESS_PDF" %(ofolder) #check on the number of "\" here
 					if PDF.endswith("_ST.pdf"):
-						small_pdf = str(pdf_folder) +"\\" + str(PDF)
+						small_pdf = "%s\\%s" %(pdf_folder,PDF)
 						if os.path.isdir(access_loc):
 							shutil.move(small_pdf, access_loc)
 						else:
 							os.mkdir(access_loc, 0755)
 							shutil.move(small_pdf, access_loc)
 					elif PDF.endswith("optimized.pdf"):
-						small_pdf = str(pdf_folder) +"\\" + str(PDF)
+						small_pdf = "%s\\%s" %(pdf_folder,PDF)
 						if os.path.isdir(access_loc):
 							shutil.move(small_pdf, access_loc)
 						else:
 							os.mkdir(access_loc, 0755)
 							shutil.move(small_pdf, access_loc)
 					elif PDF.endswith("opt.pdf"):
-						small_pdf = str(pdf_folder) +"\\" + str(PDF)
+						small_pdf = "%s\\%s" %(pdf_folder,PDF)
 						if os.path.isdir(access_loc):
 							shutil.move(small_pdf, access_loc)
 						else:
